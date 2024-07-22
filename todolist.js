6edb6af7a3cf1729613439565bd75b6017cc92a4
Jatin-B <jbaweja24@gmail.com>
2024-07-22 16:26:28 +0530

    Version-2

--- todolist.js
+++ todolist.js
@@ -5,21 +5,20 @@ renderHTML();
 function renderHTML() {
   let todoListHTML = '';
 
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
+  todoList.forEach(function(todoObject, index) {
     const { name, dueDate } = todoObject;
     const html = `
-    <div>${name}</div>
+    <div>${name}</div>  
     <div>${dueDate}</div>
     <button onclick="
-      todoList.splice(${i},1);
+      todoList.splice(${index},1);
       renderHTML();
       saveToStorage();
     " class="delete-button">Delete</button> 
     `;
 
     todoListHTML += html;
-  }
+  });
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
